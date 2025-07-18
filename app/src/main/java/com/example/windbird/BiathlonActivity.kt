9c47983c71c378470e2eee892fef4e4dfce27890
Sraaap88 <jonaprise@yahoo.ca>
2025-07-18 16:53:49 -0400

    Update BiathlonActivity.kt

--- app/src/main/java/com/example/windbird/BiathlonActivity.kt
+++ app/src/main/java/com/example/windbird/BiathlonActivity.kt
@@ -10,6 +10,7 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.view.View
 import android.view.MotionEvent
 import android.view.WindowManager
 import android.widget.LinearLayout
