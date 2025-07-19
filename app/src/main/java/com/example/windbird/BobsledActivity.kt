cae7bb73ef27ab01bcaa88c887d693069c20ddf6
Sraaap88 <jonaprise@yahoo.ca>
2025-07-18 21:38:26 -0400

    Update BobsledActivity.kt

--- app/src/main/java/com/example/windbird/BobsledActivity.kt
+++ app/src/main/java/com/example/windbird/BobsledActivity.kt
@@ -15,7 +15,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import android.view.ViewGroup
 import kotlin.math.*
-
+ 
 class BobsledActivity : Activity(), SensorEventListener {
 
     private lateinit var gameView: BobsledView
